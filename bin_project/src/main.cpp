81e67a540e6f0ea2dc49fb7187ee22ef2662e7db
charliecai <charliecai@tencent.com>
2020-05-31 21:02:26 +0800

    增加动态库编译

--- bin_project/src/main.cpp
+++ bin_project/src/main.cpp
@@ -1,4 +1,5 @@
 #include "func.h"
+#include "static_func.h"
 
 int main(int argc, char** argv)
 {
@@ -6,6 +7,7 @@ int main(int argc, char** argv)
     (void)argv;
 
     print_info();
+    print_static_info();
 
     return 0;
 }
